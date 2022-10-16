36843de45296c02ea2f1ad6d27e6705b1977b186
AirV <herve.jacotet@sfr.fr>
2022-10-16 19:25:19 +0200

    Create first entity

---
+++ src/Entity/Permissions.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PermissionsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PermissionsRepository::class)]
+class Permissions
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $mailing = null;
+
+    #[ORM\Column]
+    private ?bool $staff_planning = null;
+
+    #[ORM\Column]
+    private ?bool $locale_promote = null;
+
+    #[ORM\Column]
+    private ?bool $sale_drinks = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isMailing(): ?bool
+    {
+        return $this->mailing;
+    }
+
+    public function setMailing(bool $mailing): self
+    {
+        $this->mailing = $mailing;
+
+        return $this;
+    }
+
+    public function isStaffPlanning(): ?bool
+    {
+        return $this->staff_planning;
+    }
+
+    public function setStaffPlanning(bool $staff_planning): self
+    {
+        $this->staff_planning = $staff_planning;
+
+        return $this;
+    }
+
+    public function isLocalePromote(): ?bool
+    {
+        return $this->locale_promote;
+    }
+
+    public function setLocalePromote(bool $locale_promote): self
+    {
+        $this->locale_promote = $locale_promote;
+
+        return $this;
+    }
+
+    public function isSaleDrinks(): ?bool
+    {
+        return $this->sale_drinks;
+    }
+
+    public function setSaleDrinks(bool $sale_drinks): self
+    {
+        $this->sale_drinks = $sale_drinks;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Structures.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StructuresRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StructuresRepository::class)]
+class Structures
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $city = null;
+
+    #[ORM\Column]
+    private ?int $zip_code = null;
+
+    #[ORM\Column]
+    private ?bool $active = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}
