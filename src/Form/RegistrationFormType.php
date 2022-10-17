c168bfc43d523d147e79f639f05aa8bd43bcb6d4
AirV <herve.jacotet@sfr.fr>
2022-10-17 20:23:57 +0200

    Modification of regiter form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,12 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,14 +18,29 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            -> add('name', TextType::class, [
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le nom peut contenir qu\'un maximum de {{ limit }} caractères.'
+                    ])
+                ]
+            ])
+            -> add('first_name', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 50,
+                        'maxMessage' => 'Le prénom peut contenir qu\'un maximum de {{ limit }} caractères.'
+                    ])
+                ]
+            ])
+            -> add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de saisir un email'
+                    ])
                 ],
+                'required' => true
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -33,15 +49,29 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez ajouter un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir un minimum de {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            -> add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Franchise' => 'ROLE_FRANCHISE',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'label' => 'Rôles :',
+                'constraints' => [
+                    new NotBlank([
+                        'message'=>'Veuillez choisir un rôles !'
+                    ])
+                ],
+                'expanded' => true,
+                'multiple' => true,
             ])
         ;
     }
