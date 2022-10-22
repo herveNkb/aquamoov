4eb9bb6baa63fd56a73f6b01ec428e6363c28ab0
AirV <herve.jacotet@sfr.fr>
2022-10-22 15:57:47 +0200

    Creation first method for display all user on a table

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+// "admin" is a principal route, other methods will follow after the underscore
+// ex: admin_index or admin_machin. . .
+#[Route('/admin', name: 'app_admin_')]
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
+        return $this -> render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+
+    //    List of users registered
+    #[Route('/utilisateurs', name: 'utilisateurs')]
+    //injection de dépendance
+    public function userList(UsersRepository $users): Response
+    {
+        return $this -> render("admin/users.html.twig", [
+            // display users directly
+            'users' => $users -> findAll()
+        ]);
+    }
+
+
 }
+
+
