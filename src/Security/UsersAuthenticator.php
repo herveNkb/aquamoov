9a1697adfacd544eb6d6b76ddb32cc47eb97d196
AirV <herve.jacotet@sfr.fr>
2022-10-24 07:17:40 +0200

    Modify redirection for super-admin login

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -59,7 +59,7 @@ class UsersAuthenticator extends AbstractLoginFormAuthenticator
         $user = $token->getUser();
 
         if (in_array('ROLE_SUPER-ADMIN', $user ->getRoles())) {
-            return new RedirectResponse($this->urlGenerator->generate('app_admin_utilisateurs'));
+            return new RedirectResponse($this->urlGenerator->generate('app_admin_super-admin'));
         }
 
         if (in_array('ROLE_ADMIN', $user ->getRoles())) {
